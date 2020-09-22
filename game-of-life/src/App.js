dedb00b96f843d50487d199fe645fe75b953fea3
Your name <a.m.verdine@gmail.com>
2020-09-21 21:19:53 -0400

    Complete basic grid setup

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -2,12 +2,46 @@ import React, { useState } from "react";
 import Grid from "./Components/Grid";
 import "./App.css";
 
+const numRows = 50;
+const numCols = 50;
+
 const App = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+  console.log(grid);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h1>The Game of Life</h1>
+        <h1>The Game of Life</h1>
+        <br></br>
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          }}
+        >
+          {grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <div
+                key={`${i}-${k}`}
+                style={{
+                  width: 20,
+                  height: 20,
+                  backgroundColor: grid[i][k] ? "058ED9" : undefined,
+                  border: "solid 1px white",
+                }}
+              />
+            ))
+          )}
         </div>
       </header>
     </div>
