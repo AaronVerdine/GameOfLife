ce48e8025e1c408fe3cffa61e621dfa1e2d5083a
Your name <a.m.verdine@gmail.com>
2020-09-21 20:52:00 -0400

    Refactored into functional components with hooks

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Grid from "./Components/Grid";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div>
+          <h1>The Game of Life</h1>
+        </div>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ game-of-life/src/Components/Grid.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import "./App.css";
+
+class Grid extends React.Component {
+  render() {
+    return <div>Grid</div>;
+  }
+}
+
+export default Grid;
