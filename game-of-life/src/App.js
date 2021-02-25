58456a88d2520f70cbd5accbbdc45fc0944e9732
Your name <a.m.verdine@gmail.com>
2021-02-24 19:42:44 -0500

    Change background color to black

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -136,7 +136,7 @@ const App = () => {
                   style={{
                     width: 20,
                     height: 20,
-                    backgroundColor: grid[i][k] ? "white" : undefined,
+                    backgroundColor: grid[i][k] ? "black" : undefined,
                     border: "solid 1px white",
                   }}
                 />
